7b49af166a680f7c77081231e15ec0d8535a78db
Etan Zapinsky <etan.zapinsky@gmail.com>
2013-03-07 21:04:15 -0500

    hopefully uwsgi will work now

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,10 @@
+import sys
+import os
 from flask import Flask
+
+# needed to have uwsgi work
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 app = Flask(__name__)
 app.config.from_object('settings.defualt')
 app.config.from_envvar('WEBAPP_SETTINGS')
