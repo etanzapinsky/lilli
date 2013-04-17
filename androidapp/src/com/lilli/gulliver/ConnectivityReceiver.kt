2472619d41adb7119e34684b05029f8d44ba2a18
Sam Aarons <samaarons@gmail.com>
2013-04-17 01:33:33 -0400

    Permissions

---
+++ androidapp/src/com/lilli/gulliver/ConnectivityReceiver.kt
@@ -0,0 +1,11 @@
+package com.lilli.gulliver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ConnectivityReceiver : BroadcastReceiver() {
+    public override fun onReceive(context: Context?, intent: Intent?) {
+        return
+    }
+}
