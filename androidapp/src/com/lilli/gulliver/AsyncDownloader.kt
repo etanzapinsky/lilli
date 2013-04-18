5fb0548f49859ce969f4fce8dc26fca7be1e216a
Etan Zapinsky <etan.zapinsky@gmail.com>
2013-04-17 21:32:36 -0400

    fixed main ui thread crash

--- androidapp/src/com/lilli/gulliver/AsyncDownloader.kt
+++ androidapp/src/com/lilli/gulliver/AsyncDownloader.kt
@@ -7,30 +7,30 @@ import android.content.Context
 import android.content.ContentValues
 import android.widget.TextView
 
-class AsyncDownloader(val downloader : Downloader?, val context : Context?, val dbHelper : StatDbHelper?, val response : TextView?) : AsyncTask<String, Int, InputStream?>() {
+class AsyncDownloader(val downloader : Downloader?, val context : Context?, val dbHelper : StatDbHelper?, val response : TextView?) : AsyncTask<String, Int, Long?>() {
     var startTime : Long = 0
 
-    protected override fun doInBackground(vararg p0 : String?) : InputStream? {
+    protected override fun doInBackground(vararg p0 : String?) : Long? {
         val first = p0.get(0)
         startTime = System.nanoTime()
 
         if (first != null) {
-            return downloader?.getData(first, context)
+            val result = downloader?.getData(first, context)
+            val file = File(context?.getCacheDir(), "temp")
+            file.writeBytes(result?.readBytes())
+            return file.length()
         }
 
         return null
     }
 
-    protected override fun onPostExecute(result : InputStream?) {
+    protected override fun onPostExecute(result : Long?) {
         val message = if (result != null) {
-            val file = File(context?.getCacheDir(), "temp")
-            file.writeBytes(result.readBytes())
-
             val elapsed = System.nanoTime() - startTime
 
             val values = ContentValues()
             values.put(StatContract.StatEntry.COLUMN_NAME_NAME, "something")
-            values.put(StatContract.StatEntry.COLUMN_NAME_SIZE, file.length())
+            values.put(StatContract.StatEntry.COLUMN_NAME_SIZE, result)
             values.put(StatContract.StatEntry.COLUMN_NAME_TIME, elapsed)
             values.put(StatContract.StatEntry.COLUMN_NAME_METHOD, downloader.toString())
 
