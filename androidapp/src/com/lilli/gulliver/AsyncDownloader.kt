fadc3f949fdc88d90f70d858321be7fb97d58490
Etan Zapinsky <etan.zapinsky@gmail.com>
2013-04-22 15:59:45 -0400

    bookeeping changes

--- androidapp/src/com/lilli/gulliver/AsyncDownloader.kt
+++ androidapp/src/com/lilli/gulliver/AsyncDownloader.kt
@@ -5,28 +5,28 @@ import android.content.Context
 import android.content.ContentValues
 import android.widget.TextView
 
-class AsyncDownloader(val downloader : Downloader?, val context : Context?, val options : Map<String, String?>?, val dbHelper : StatDbHelper?, val response : TextView?) : AsyncTask<String, Int, Long?>() {
+class AsyncDownloader(val downloader : Downloader?, val context : Context?, val options : Map<String, String?>?, val dbHelper : StatDbHelper?, val response : TextView?) : AsyncTask<String, Int, Pair<String?, Long?>?>() {
     var startTime : Long = 0
 
-    protected override fun doInBackground(vararg p0 : String?) : Long? {
+    protected override fun doInBackground(vararg p0 : String?) : Pair<String?, Long?>? {
         val first = p0.get(0)
         startTime = System.nanoTime()
 
         if (first != null) {
             val result = downloader?.getData(first, context, options)
-            return result?.length()
+            return Pair(first, result?.length())
         }
 
         return null
     }
 
-    protected override fun onPostExecute(result : Long?) {
+    protected override fun onPostExecute(result : Pair<String?, Long?>?) {
         val message = if (result != null) {
             val elapsed = System.nanoTime() - startTime
 
             val values = ContentValues()
-            values.put(StatContract.StatEntry.COLUMN_NAME_NAME, "something")
-            values.put(StatContract.StatEntry.COLUMN_NAME_SIZE, result)
+            values.put(StatContract.StatEntry.COLUMN_NAME_NAME, result.first)
+            values.put(StatContract.StatEntry.COLUMN_NAME_SIZE, result.second)
             values.put(StatContract.StatEntry.COLUMN_NAME_TIME, elapsed)
             values.put(StatContract.StatEntry.COLUMN_NAME_METHOD, downloader.toString())
 
